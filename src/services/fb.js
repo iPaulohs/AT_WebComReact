4cebebc27db388ef114d35af9ca64720a20a5fa2
Paulo Henrique <paulo.hsantos@al.infnet.edu.br>
2023-06-13 13:15:23 -0300

    Sincronia

---
+++ src/services/fb.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBd_6pK5EG-xT_CE9ZLHPuwO26iydOC8Cc",
+    authDomain: "at-react-phs.firebaseapp.com",
+    projectId: "at-react-phs",
+    storageBucket: "at-react-phs.appspot.com",
+    messagingSenderId: "245188388766",
+    appId: "1:245188388766:web:c5af760f40b0382e6ad88f"
+  };
+
+  export const app = initializeApp(firebaseConfig); 
